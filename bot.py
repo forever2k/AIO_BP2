40e1354c58654df055ef87e375f49ce4da38788c
forever2k <forever2k@yandex.ru>
2021-08-08 12:12:14 +0300

    new commit

--- bot.py
+++ bot.py
@@ -14,19 +14,18 @@ from app.handlers.common import register_handlers_common
 
 # test_group = -1001153348142
 # test = -1001364950026
-# me = os.getenv('me')
-#
-#
-#
-# TOKEN = os.getenv('TOKEN')
-# PROJECT_NAME = os.getenv('PROJECT_NAME')
-#
-# WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'
-# WEBHOOK_PATH = '/' + TOKEN
-# WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-#
-# WEBAPP_HOST = '0.0.0.0'
-# WEBAPP_PORT = os.environ.get('PORT')
+
+me = os.getenv('me')
+
+TOKEN = os.getenv('TOKEN')
+PROJECT_NAME = os.getenv('PROJECT_NAME')
+
+WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'
+WEBHOOK_PATH = '/' + TOKEN
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -79,7 +78,7 @@ async def main():
 
 async def on_startup(dp):
     logging.warning('Starting connection')
-    await bot.set_webhook(config.ask_bot.WEBHOOK_URL)
+    await bot.set_webhook(WEBHOOK_URL)
     # asyncio.create_task(bot_schedule())
 
 
@@ -88,9 +87,9 @@ async def on_shutdown(dp):
 
 
 if __name__ == '__main__':
-    start_webhook(dispatcher=dp, webhook_path=config.ask_bot.WEBHOOK_PATH,
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
                   on_startup=on_startup, on_shutdown=on_shutdown,
-                  host=config.ask_bot.WEBAPP_HOST, port=config.ask_bot.WEBAPP_PORT, skip_updates=True)
+                  host=WEBAPP_HOST, port=WEBAPP_PORT, skip_updates=True)
     asyncio.run(main())
 
 

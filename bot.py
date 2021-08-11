c5d6f9a3c81ee19c432afcc4419dda804b42731f
forever2k <forever2k@yandex.ru>
2021-08-11 19:23:47 +0300

    new commit

--- bot.py
+++ bot.py
@@ -63,8 +63,8 @@ async def set_commands(bot: Bot):
 
 
 
-dp.dp.register_message_handler(cmd_start22, commands="start22", state="*")
-dp.dp.register_message_handler(cmd_start, commands="start", state="*")
+dp.register_message_handler(cmd_start22, commands="start22", state="*")
+dp.register_message_handler(cmd_start, commands="start", state="*")
 dp.register_message_handler(cmd_cancel, commands="cancel", state="*")
 dp.register_message_handler(cmd_cancel, Text(equals="отмена", ignore_case=True), state="*")
 dp.register_message_handler(ask_start, commands=["ask"], state="*")

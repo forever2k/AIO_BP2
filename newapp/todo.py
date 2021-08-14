7f617e678fd41f9cde462c21b49bd0bf5e581257
forever2k <forever2k@yandex.ru>
2021-08-15 00:43:44 +0300

    new commit

--- newapp/todo.py
+++ newapp/todo.py
@@ -2,7 +2,7 @@ from random import randint
 
 def generate_number(id):
     id_generate = str(id) + "_"
-    for i in range(0, 10):
+    for i in range(0, 6):
         id_generate = id_generate + str(randint(1, 10))
     return id_generate
 

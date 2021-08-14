2d01b9d81fc85fe86279224fa7f41771474fdd31
forever2k <forever2k@yandex.ru>
2021-08-14 13:08:59 +0300

    new commit

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram.types import BotCommand
+from aiogram.utils.executor import start_webhook
+import mysql.connector
+from random import randint
+from aiogram.utils.exceptions import BotBlocked
+
+
+test_group = -1001153348142
+test = -1001364950026
+
+me = os.getenv('me')
+
+TOKEN = os.getenv('TOKEN')
+PROJECT_NAME = os.getenv('PROJECT_NAME')
+
+WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'
+WEBHOOK_PATH = '/' + TOKEN
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
+
+
+dbase = mysql.connector.connect(
+        host=os.getenv('myhost'),
+        user=os.getenv('myuser'),
+        passwd=os.getenv('mypass'),
+        port="3306",
+        database="bqcbwpmrbqj7ghxx")
+
---
+++ get_data.py
@@ -0,0 +1,79 @@
+import logging
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import exceptions
+from config import dbase, test_group, me
+from app import bot
+
+# available_questions = ["вопрос1", "вопрос3", "вопрос3"]
+# available_answers = ["ответ1", "ответ2", "ответ3"]
+
+
+cursor = dbase.cursor()
+
+class GetData(StatesGroup):
+    waiting_for_question = State()
+    waiting_for_answer = State()
+
+
+async def start_session(call: types.CallbackQuery):
+    # keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # for name in available_questions:
+    #     keyboard.add(name)
+    # await call.message.answer("Send me your question:", reply_markup=keyboard)
+    await call.message.answer("Send me your question:")
+    await GetData.waiting_for_question.set()
+    # await call.answer(text="Thanks!", show_alert=True)
+    # или просто await call.answer()
+
+
+async def get_question(message: types.Message, state: FSMContext):
+    if len(message.text) < 5:
+        await message.answer("Пожалуйста, напишите корректный вопрос, используя клавиатуру ниже.")
+        return
+    await state.update_data(chosen_question=message.text.lower())
+
+    # keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # for size in available_answers:
+    #     keyboard.add(size)
+    await GetData.waiting_for_answer.set()
+    # await message.answer("Now write your ANSWER", reply_markup=keyboard)
+    await message.answer("Now write your ANSWER")
+
+
+async def get_answer(message: types.Message, state: FSMContext):
+    if len(message.text) < 5:
+        await message.answer("Пожалуйста, напишите вопрос, используя клавиатуру ниже.")
+        return
+    await state.update_data(chosen_answer=message.text.lower())
+    user_data = await state.get_data()
+    await message.answer(f"Вы написали вопрос: {user_data['chosen_question']}.\n"
+                         f"Вы написали ответ: {user_data['chosen_answer']}.\n"
+                         f"Попробуйте теперь задать еще вопрос: /start", reply_markup=types.ReplyKeyboardRemove())
+
+    user_id = message.from_user.id
+    try:
+
+        sql = "INSERT INTO users (user_id, QUESTION, ANSWER) \
+                                                          VALUES (%s, %s, %s)"
+        val = (user_id, user_data['chosen_question'], user_data['chosen_answer'])
+        cursor.execute(sql, val)
+        dbase.commit()
+
+    except Exception as e:
+        await message.reply(message, 'Something went wrong.. Please contact the admin')
+
+    try:
+        await message.answer("it`s the last part")
+        await bot.send_message(test_group, "A new message has been received", disable_notification=False)
+        await bot.send_message(me, 'New question was recevied')
+
+    except exceptions.BotBlocked:
+        logging.error(f"Target [ID:{test_group}]: blocked by user")
+
+
+
+
+
+    await state.finish()

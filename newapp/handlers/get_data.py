d1efea10c524015658c6c8f5cd5bc0d46592b8a4
forever2k <forever2k@yandex.ru>
2021-08-15 23:17:42 +0300

    new commit

--- newapp/handlers/get_data.py
+++ newapp/handlers/get_data.py
@@ -61,8 +61,8 @@ async def ask_answer(message: types.Message):
 
 
 async def get_answer(call: types.CallbackQuery):
-    await GetData.waiting_for_write_answer.set()
     await call.answer("Now write your ANSWER")
+    await GetData.waiting_for_write_answer.set()
 
 
 async def write_answer(message: types.Message, state: FSMContext):

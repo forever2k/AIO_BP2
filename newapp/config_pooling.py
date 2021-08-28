9bd187fd3aa69670f2852bfe24320f66bec62698
forever2k <forever2k@yandex.ru>
2021-08-28 14:34:04 +0300

    new commit

---
+++ newapp/config_pooling.py
@@ -0,0 +1,18 @@
+import os
+import mysql.connector
+
+
+
+# test_group = -1001153348142
+# test = -1001364950026
+
+me = 227722043
+
+
+TOKEN = "1619381009:AAEAlXyTON2xG6H-m2OaCAq96i63bdZyRpA"
+dbase = mysql.connector.connect(
+        host="yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+        user="n4ptgcx1ahrt578v",
+        passwd="sgn0mrw2lwgts4ai",
+        port="3306",
+        database="bqcbwpmrbqj7ghxx")

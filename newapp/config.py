6c5e51f5ace614eec8a091308492e3cd854e93d0
forever2k <forever2k@yandex.ru>
2022-03-25 18:46:55 +0300

    new commit

--- newapp/config.py
+++ newapp/config.py
@@ -1,35 +1,25 @@
 import os
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher, FSMContext
-from aiogram.types import BotCommand
-from aiogram.utils.executor import start_webhook
 import mysql.connector
-from random import randint
-from aiogram.utils.exceptions import BotBlocked
 
 
-USE_WEBHOOK = False
+USE_WEBHOOK = True
 
-test_group = -1001153348142
-test_channel = -1001364950026
+test_group = os.getenv('test_group')
+test_channel = os.getenv('test_channel')
 
 
 if USE_WEBHOOK == False:
-        from config_pooling import me, TOKEN, dbase
+        from config_pooling import TOKEN, dbase
         TOKEN = TOKEN
         dbase = dbase
 
 else:
         me = os.getenv('me')
-
         TOKEN = os.getenv('TOKEN')
-
         PROJECT_NAME = os.getenv('PROJECT_NAME')
-
         WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'
         WEBHOOK_PATH = '/' + TOKEN
         WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
         WEBAPP_HOST = '0.0.0.0'
         WEBAPP_PORT = os.environ.get('PORT')
 
